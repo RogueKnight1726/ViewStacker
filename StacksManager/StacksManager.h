47610bb5bd6748fa28c9be1bc1b0dd00895efbba
swathys@nexteducation.in <swathys@nexteducation.in>
2020-08-01 12:35:00 +0530

    StackManager Framework created with tap gesture added to go to back and swipe gesture

---
+++ StacksManager/StacksManager.h
@@ -0,0 +1,19 @@
+//
+//  StacksManager.h
+//  StacksManager
+//
+//  Created by Next on 01/08/20.
+//  Copyright © 2020 Next. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StacksManager.
+FOUNDATION_EXPORT double StacksManagerVersionNumber;
+
+//! Project version string for StacksManager.
+FOUNDATION_EXPORT const unsigned char StacksManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StacksManager/PublicHeader.h>
+
+
--- ViewStacker/Controllers/HomeController.swift
+++ ViewStacker/Controllers/HomeController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import StacksManager
 
 class HomeController: UIViewController{
     
